d1b768e57b775c9ed3d0452ae770dc91c27e6bb3
Prabowo Murti <prabowo.murti@gmail.com>
2024-08-22 22:33:57 +0700

    feat: hide DebugPanel / menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { ContextMenu, DefaultActionsMenu, DefaultHelpMenu, DefaultHelperButtons, DefaultKeyboardShortcutsDialog, DefaultMainMenu, DefaultMenuPanel, DefaultMinimap, DefaultNavigationPanel, DefaultPageMenu, DefaultQuickActions, DefaultSharePanel, DefaultStylePanel, DefaultToolbar, DefaultTopPanel, DefaultZoomMenu, TLUiComponents, Tldraw } from 'tldraw'
 import './App.css'
-import { Tldraw } from 'tldraw'
+
+// Hide the DebugPanel and DebugMenu components
+const components: Required<TLUiComponents> = {
+	ContextMenu: ContextMenu,
+	ActionsMenu: DefaultActionsMenu,
+	HelpMenu: DefaultHelpMenu,
+	ZoomMenu: DefaultZoomMenu,
+	MainMenu: DefaultMainMenu,
+	Minimap: DefaultMinimap,
+	StylePanel: DefaultStylePanel,
+	PageMenu: DefaultPageMenu,
+	NavigationPanel: DefaultNavigationPanel,
+	Toolbar: DefaultToolbar,
+	KeyboardShortcutsDialog: DefaultKeyboardShortcutsDialog,
+	QuickActions: DefaultQuickActions,
+	HelperButtons: DefaultHelperButtons,
+	DebugPanel: null,
+	DebugMenu: null,
+	SharePanel: DefaultSharePanel,
+	MenuPanel: DefaultMenuPanel,
+	TopPanel: DefaultTopPanel,
+	CursorChatBubble: null,
+}
 
 function App() {
-  return (
+	return (
 		<div style={{ position: 'fixed', inset: 0 }}>
-			<Tldraw />
+			<Tldraw components={components} />
 		</div>
 	)
 }
